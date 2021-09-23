ebb014a10a69041026e1d0fe5da721d1e31aae7e
Maverick Perrollaz <maverick.perrollaz@pm.me>
2021-09-23 09:14:37 +0200

    create controllers

---
+++ controllers/category.js
@@ -0,0 +1,75 @@
+function createCategory(req, res) {
+    let Category = require('../models/category');
+    let newCategory = Category ({
+        title: req.body.title,
+        description : req.body.description
+    });
+  
+    newCategory.save()
+    .then((savedCategory) => {
+
+        //send back the created Category
+        res.json(savedCategory);
+            
+    }, (err) => {
+        res.status(400).json(err)
+    });
+
+}
+
+function readCategories(req, res) {
+
+    let Category = require("../models/category");
+
+    Category.find({})
+    .then((categories) => {
+        res.status(200).json(categories);
+    }, (err) => {
+        res.status(500).json(err);
+    });
+ }
+
+function readCategory(req, res) {
+
+    let Category = require("../models/category");
+
+    Category.findById({_id : req.params.id})
+    .then((category) => {
+        res.status(200).json(category);
+    }, (err) => {
+        res.status(500).json(err);
+    });
+ }
+
+function updateCategory(req, res) {
+
+    let Category = require("../models/category");
+
+    Category.findByIdAndUpdate({_id: req.params.id}, 
+        {title : req.body.title, 
+        description : req.body.description}, 
+        {new : true})
+    .then((updatedCategory) => {
+        res.status(200).json(updatedCategory);
+    }, (err) => {
+        res.status(500).json(err);
+    });
+}
+
+function deleteCategory(req, res) {
+
+    let Category = require("../models/category");
+
+    Category.findOneAndRemove({_id : req.params.id})
+    .then((deletedCategory) => {
+        res.status(200).json(deletedCategory);
+    }, (err) => {
+        res.status(500).json(err);
+    });
+ }
+
+module.exports.create = createCategory;
+module.exports.reads = readCategories;
+module.exports.read = readCategory;
+module.exports.delete = deleteCategory;
+module.exports.update = updateCategory;
---
+++ controllers/product.js
@@ -0,0 +1,75 @@
+function createProduct(req, res) {
+    let Product = require('../models/product');
+    let newProduct = Product ({
+        title: req.body.title,
+        description : req.body.description
+    });
+  
+    newProduct.save()
+    .then((savedProduct) => {
+
+        //send back the created Product
+        res.json(savedProduct);
+            
+    }, (err) => {
+        res.status(400).json(err)
+    });
+
+}
+
+function readProducts(req, res) {
+
+    let Product = require("../models/product");
+
+    Product.find({})
+    .then((products) => {
+        res.status(200).json(products);
+    }, (err) => {
+        res.status(500).json(err);
+    });
+ }
+
+function readProduct(req, res) {
+
+    let Product = require("../models/product");
+
+    Product.findById({_id : req.params.id})
+    .then((product) => {
+        res.status(200).json(product);
+    }, (err) => {
+        res.status(500).json(err);
+    });
+ }
+
+function updateProduct(req, res) {
+
+    let Product = require("../models/product");
+
+    Product.findByIdAndUpdate({_id: req.params.id}, 
+        {title : req.body.title, 
+        description : req.body.description}, 
+        {new : true})
+    .then((updatedProduct) => {
+        res.status(200).json(updatedProduct);
+    }, (err) => {
+        res.status(500).json(err);
+    });
+}
+
+function deleteProduct(req, res) {
+
+    let Product = require("../models/product");
+
+    Product.findOneAndRemove({_id : req.params.id})
+    .then((deletedProduct) => {
+        res.status(200).json(deletedProduct);
+    }, (err) => {
+        res.status(500).json(err);
+    });
+ }
+
+module.exports.create = createProduct;
+module.exports.reads = readProducts;
+module.exports.read = readProduct;
+module.exports.delete = deleteProduct;
+module.exports.update = updateProduct;
