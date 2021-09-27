a6b492946eaa025e131da0ba8231e3ba68853d3f
Maverick Perrollaz <maverick.perrollaz@pm.me>
2021-09-27 13:46:41 +0200

    update shop route

--- routes/shop.js
+++ routes/shop.js
@@ -5,6 +5,41 @@ const router = require('express').Router();
 const controller = require('../controllers/shop');
 
 
+//CREATE
+router.post("/shop", (req, res) => {
+
+    controller.create(req, res);
+
+});
+
+//READ
+router.get("/shops", (req, res) => {
+    
+    controller.reads(req, res);
+
+});
+
+router.get("/shop/:id", (req, res) => {
+    
+    controller.read(req, res);
+
+});
+
+//UPDATE
+router.put("/shop/:id", (req, res) => {
+    
+    controller.update(req, res);
+
+});
+
+//DELETE
+router.delete("/shop/:id", (req, res) => {
+    
+    controller.delete(req, res);
+
+});
+
+
 //READ
 router.get("/", (req, res) => {
     
